9c5b4408e0e1f2003e42e86b8ffcaa84c8867221
sagar-spice <99943360+sagar-spice@users.noreply.github.com>
2022-02-25 14:56:46 +0530

    you tube player update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "34a9dbcb5276b81662aa138c90f26d60",
-"index.html": "cab5f7fca7241a89e465127b4fb9a304",
-"/": "cab5f7fca7241a89e465127b4fb9a304",
-"main.dart.js": "074692c248db1567cb568e96dc6e1278",
+"index.html": "74f2f1d13a6f18073e00933dd9b4bafa",
+"/": "74f2f1d13a6f18073e00933dd9b4bafa",
+"main.dart.js": "08084accdf7b40224e137c516f05b814",
 "script.js": "99d927bb2603e6a524c055c98d662855",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
